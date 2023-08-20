03637e9986774e0e13f324d19fbf80c5ce6d9069
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-20 12:09:23 -0300

    criando projeto

---
+++ src/test/java/com/elbertribeiro/kmovieskseries/KmovieskseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elbertribeiro.kmovieskseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmovieskseriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
