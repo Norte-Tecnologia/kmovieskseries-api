459755638d1c16e26008417f9b46b18c61c2d4aa
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-08-20 20:30:45 -0300

    otimizando imports

--- src/main/java/com/elbertribeiro/kmovieskseries/serie/SerieRepository.java
+++ src/main/java/com/elbertribeiro/kmovieskseries/serie/SerieRepository.java
@@ -1,6 +1,5 @@
 package com.elbertribeiro.kmovieskseries.serie;
 
-import com.elbertribeiro.kmovieskseries.serie.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/elbertribeiro/kmovieskseries/serie/SerieService.java
+++ src/main/java/com/elbertribeiro/kmovieskseries/serie/SerieService.java
@@ -1,7 +1,5 @@
 package com.elbertribeiro.kmovieskseries.serie;
 
-import com.elbertribeiro.kmovieskseries.serie.Serie;
-import com.elbertribeiro.kmovieskseries.serie.SerieRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
