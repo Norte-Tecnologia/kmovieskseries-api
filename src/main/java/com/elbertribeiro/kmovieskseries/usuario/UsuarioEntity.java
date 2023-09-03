a7ca623eca4c51eae914935741c42554d11f9279
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-09-03 13:36:07 -0300

    ignorando arquivo db

--- src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioEntity.java
+++ src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class UsuarioEntity implements Usuario{
+public class UsuarioEntity implements Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
