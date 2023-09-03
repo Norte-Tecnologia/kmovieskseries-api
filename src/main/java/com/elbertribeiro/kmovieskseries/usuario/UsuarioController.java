7a6844553a6573ef762f670a9ec43e584d1c7a24
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-09-03 12:54:13 -0300

    criando id user

--- src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioController.java
+++ src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioController.java
@@ -1,10 +1,9 @@
 package com.elbertribeiro.kmovieskseries.usuario;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +11,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @GetMapping
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         Map<String, Object>  map = new HashMap<>();
@@ -19,5 +24,12 @@ public class UsuarioController {
         map.put("email", principal.getAttribute("email"));
         map.put("id", principal.getAttribute("sub"));
         return map;
+    }
+
+    @PostMapping
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public Integer createUser() {
+        return usuarioService.createIdUser();
     }
 }
---
+++ src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioService.java
@@ -0,0 +1,10 @@
+package com.elbertribeiro.kmovieskseries.usuario;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    public Integer createIdUser(){
+        return 1;
+    }
+}
