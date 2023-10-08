d49bb35951733c14bf4c7d0d4bc43434a73ea246
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-10-08 11:11:12 -0300

    refactory: criando arquivo para mensagem

--- src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioDto.java
+++ src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioDto.java
@@ -1,11 +1,9 @@
 package com.elbertribeiro.kmovieskseries.usuario;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public class UsuarioDto {
-    @NotNull
-    @NotEmpty(message = "{usuario.campo.obrigatorio}")
+    @NotBlank(message = "{usuario.nome.campo.obrigatorio}")
     private String name;
 
     public String getName() {
