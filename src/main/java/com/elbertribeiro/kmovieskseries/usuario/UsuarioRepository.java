7ce204ef82e04421f946b6b3d9ebea8e355d4cea
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-10-07 22:02:09 -0300

    refactory: ajustando coleção de usuario

--- src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioRepository.java
+++ src/main/java/com/elbertribeiro/kmovieskseries/usuario/UsuarioRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends MongoRepository<Usuario, Integer> {
+public interface UsuarioRepository extends MongoRepository<Usuario, String> {
 }
