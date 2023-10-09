5ee3eb9ab9af71d5e6a5908f3d951c1767cd75ea
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-10-09 11:40:59 -0300

    refactory: criando dto para criacao da serie

---
+++ src/main/java/com/elbertribeiro/serie/NovaSerieDto.java
@@ -0,0 +1,80 @@
+package com.elbertribeiro.serie;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * DTO for {@link Serie}
+ */
+public class NovaSerieDto {
+    @NotBlank(message = "O titulo da obra não pode ser nulo")
+    private String titulo;
+    @NotBlank
+    private PlataformaStreaming streaming;
+    @NotBlank
+    private String tipo;
+    @NotBlank
+    private Integer temporadas;
+    @NotBlank
+    private Integer episodiosPorTemporada;
+    @NotBlank
+    private Integer idUsuario;
+    @NotBlank
+    private Boolean assistido;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public PlataformaStreaming getStreaming() {
+        return streaming;
+    }
+
+    public void setStreaming(PlataformaStreaming streaming) {
+        this.streaming = streaming;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(Integer temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public Integer getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(Integer episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Boolean getAssistido() {
+        return assistido;
+    }
+
+    public void setAssistido(Boolean assistido) {
+        this.assistido = assistido;
+    }
+}
+
