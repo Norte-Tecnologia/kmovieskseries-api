33e085532c1b3f353320a4fbc99e9572f240c2a2
Henrique <lhenrique.lh000@gmail.com>
2023-10-12 15:47:56 -0300

    refactor: alterando tipo da variável tipo

--- src/main/java/com/elbertribeiro/serie/NovaSerieDto.java
+++ src/main/java/com/elbertribeiro/serie/NovaSerieDto.java
@@ -1,6 +1,7 @@
 package com.elbertribeiro.serie;
 
 import com.elbertribeiro.plataforma.PlataformaStreaming;
+import com.elbertribeiro.tipo.Tipo;
 import jakarta.validation.constraints.NotBlank;
 
 /**
@@ -12,7 +13,7 @@ public class NovaSerieDto {
     @NotBlank
     private PlataformaStreaming streaming;
     @NotBlank
-    private String tipo;
+    private Tipo tipo;
     @NotBlank
     private Integer temporadas;
     @NotBlank
@@ -38,11 +39,11 @@ public class NovaSerieDto {
         this.streaming = streaming;
     }
 
-    public String getTipo() {
+    public Tipo getTipo() {
         return tipo;
     }
 
-    public void setTipo(String tipo) {
+    public void setTipo(Tipo tipo) {
         this.tipo = tipo;
     }
 
