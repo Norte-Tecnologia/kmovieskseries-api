b21ad43ed16cde3570c879e02c0c64e9e8c79144
Henrique <lhenrique.lh000@gmail.com>
2023-10-12 12:24:07 -0300

    feat: alterando modificador do conversor

--- src/main/java/com/elbertribeiro/serie/SerieConvert.java
+++ src/main/java/com/elbertribeiro/serie/SerieConvert.java
@@ -1,30 +1,42 @@
 package com.elbertribeiro.serie;
 
 
+import com.elbertribeiro.plataforma.PlataformaService;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SerieConvert {
-    private SerieConvert() {
+
+    private final PlataformaService plataformaService;
+
+    public SerieConvert(PlataformaService plataformaService) {
+        this.plataformaService = plataformaService;
     }
 
-    public static SerieDto serieToDto(Serie serie) {
+
+    public SerieDto serieToDto(Serie serie) {
         return new SerieDto(
                 serie.getTitulo(),
                 serie.getPlataforma(),
+                plataformaService.buscaNomeById(serie.getPlataforma()),
                 serie.getTipo(),
                 serie.getTemporadas(),
                 serie.getEpisodiosPorTemporada(),
                 serie.getUsuario(),
-                serie.getAssistido()
+                serie.getAssistido(),
+                plataformaService.buscaUrlLogoById(serie.getPlataforma())
         );
     }
 
-    public static Serie serieToEntity(NovaSerieDto serie) {
+    public Serie serieToEntity(NovaSerieDto serie) {
         Serie serieEntity = new Serie();
-        serieEntity.setPlataforma(serie.getStreaming().getNome());
+        serieEntity.setPlataforma(serie.getStreaming().getId());
         serieEntity.setTipo(serie.getTipo());
         serieEntity.setTitulo(serie.getTitulo());
         serieEntity.setTemporadas(serie.getTemporadas());
         serieEntity.setEpisodiosPorTemporada(serie.getEpisodiosPorTemporada());
         serieEntity.setUsuario(serie.getIdUsuario());
+        serieEntity.setAssistido(serie.getAssistido());
         return serieEntity;
     }
 }
